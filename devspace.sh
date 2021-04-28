6a4519340d0609323812c9dae06bd306c022805e
Ryan Will <ryanwilldev@gmail.com>
2021-04-28 14:42:29 -0500

    Removes special handling of work/personal machines
    Uses -d to prevent newly opened windows from becoming the active window

--- devspace.sh
+++ devspace.sh
@@ -12,19 +12,10 @@ then
       tmux attach
     else
       sleep 1
-      if [[  -z $WORK_MACHINE  ]]; then
-        tmux new -c "/Users/${USER}/notes/main" -n "notes" nvim \;\
-          send-keys -t 0 ",ww" \;\
-          send-keys -t 0 ",aft" \;\
-          new-window -c "/Users/${USER}/Projects" \;\
-          select-window -l
-      else
         tmux new -c "/Users/${USER}/notes/main" -n "notes" nvim \;\
           send-keys -t 0 ",w,w" \;\
           send-keys -t 0 ",aft" \;\
-          new-window -c "/Users/${USER}/Projects" \;\
-          select-window -l
-      fi
+          new-window -d -c "/Users/${USER}/Projects"
     fi
   else
     echo "You don't have tmux installed : ("
