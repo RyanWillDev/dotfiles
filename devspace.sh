a6bfec674a759f729a5444568f812bdd2a7941ba
Ryan Will <ryanwilldev@gmail.com>
2022-01-14 09:26:21 -0600

    changes directory

--- devspace.sh
+++ devspace.sh
@@ -11,7 +11,7 @@ then
     then
       tmux attach
     else
-      tmux new -c "/Users/${USER}/notes/main" -n "notes" nvim \;\
+      tmux new -c "/Users/${USER}/notes" -n "notes" nvim \;\
         send-keys -t 0 ",w,w" \;\
         send-keys -t 0 ",aft" \;\
         new-window -d -c "/Users/${USER}/Projects"
