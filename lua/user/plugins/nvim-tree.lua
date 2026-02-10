feac178267dbb41523f57c3347753d1668df4233
Ryan Will <ryanwilldev@gmail.com>
2026-02-10 12:36:24 -0600

    Allows collapsing whole tree

--- lua/user/plugins/nvim-tree.lua
+++ lua/user/plugins/nvim-tree.lua
@@ -54,6 +54,7 @@ function M.config(_plugin, _opts)
       vim.keymap.set("n", "<leader>v", api.node.open.vertical, opts("Open vertical split"))
       vim.keymap.set("n", "<leader>s", api.node.open.horizontal, opts("Open horizontal split"))
       vim.keymap.set("n", "x", api.node.navigate.parent_close, opts("Close parent directory"))
+      vim.keymap.set("n", "<BS>", api.tree.collapse_all, opts("Close parent directory"))
     end
   })
 end
