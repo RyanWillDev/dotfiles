77506b43e56cc195b4ce9aec600892a3cac977fa
Ryan Will <ryanwilldev@gmail.com>
2026-03-07 13:57:04 -0600

    ignores python dependency directories

--- lua/user/plugins/fzf.lua
+++ lua/user/plugins/fzf.lua
@@ -4,7 +4,7 @@ local vim = _G.vim -- Let lua lsp know that vim is global
 function M.config(_plugin, opts)
   local fzf = require('fzf-lua')
 
-  local ignore_patterns = [[-g "!.git" -g "!_build" -g "!deps" -g "!node_modules" -g "!.elixir_ls" -g "!cover/**/*"]]
+  local ignore_patterns = [[-g "!.git" -g "!_build" -g "!deps" -g "!node_modules" -g "!.elixir_ls" -g "!cover/**/*" -g "!python/venv/**/*"]]
 
   fzf.setup({
     grep = {
