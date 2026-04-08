f437b47d4f76cf088c3ce1cbe923270c2ed65b2a
Ryan Will <ryanwilldev@gmail.com>
2026-04-08 17:36:50 -0500

    fixes nvim checkhealth warnings

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -3,6 +3,10 @@ local vim = _G.vim
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
 
 vim.cmd("set noshiftround")
 vim.o.autoread = true
