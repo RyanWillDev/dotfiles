459679bd78b02419b009e68fe6380291cd5fcac3
Ryan Will <ryanwilldev@gmail.com>
2026-07-07 19:00:09 -0500

    fixes snippet jumps

--- lua/user/plugins/lsp.lua
+++ lua/user/plugins/lsp.lua
@@ -13,8 +13,22 @@ function M.config()
 
 
     vim.keymap.set({ "i" }, "<C-Y>", function() ls.expand() end, { silent = true })
-    vim.keymap.set({ "i", "s" }, "<C-N>", function() ls.jump(1) end, { silent = true })
-    vim.keymap.set({ "i", "s" }, "<C-P>", function() ls.jump(-1) end, { silent = true })
+    vim.keymap.set({ "i", "s" }, "<Tab>", function()
+      if ls.jumpable(1) then
+        ls.jump(1)
+      else
+        return '<Tab>'
+      end
+    end, { silent = true, expr = true })
+
+    vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
+      if ls.jumpable(-1) then
+        ls.jump(-1)
+      else
+        return '<S-Tab>'
+      end
+    end, { silent = true, expr = true })
+
 
     vim.keymap.set({ "i", "s" }, "<C-C>", function()
       if ls.choice_active() then
